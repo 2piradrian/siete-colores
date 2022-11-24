bd5a3119856ea2f9ed3a393f55bdb3bfec27162b
Adrian Rodriguez <rodriguezcadr@gmail.com>
2022-11-24 19:18:39 -0300

    project setup

---
+++ src/Styles/Theme.js
@@ -0,0 +1,10 @@
+export const originalTheme = {
+	green: "#C3F0CA",
+	darkPink: "#FF8BA7",
+	lightPink: "#FFC6C7",
+	black: "#FF8BA7",
+	darkBrown: "#33272A",
+	lightBrown: "#33272A",
+	cream: "#FAEEE7",
+	white: "#FAEEE7",
+};
