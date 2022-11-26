599f26cd9a972e9a1b33779865f93038cf790f83
Adrian Rodriguez <rodriguezcadr@gmail.com>
2022-11-26 14:09:24 -0300

    project setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { GlobalStyle } from "./Styles/GlobalStyles";
+import { GlobalStyle } from "./Themes/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
