c1599fd1d9e8a3e31bff739e1ad80e3c3b033e21
Adrian Rodriguez <rodriguezcadr@gmail.com>
2022-12-07 14:46:46 -0300

    useNavigate in cart

--- src/Routes/Cart.js
+++ src/Routes/Cart.js
@@ -2,13 +2,13 @@ import React from "react";
 import { StyledLink } from "../Components/General/Global";
 import { Close } from "../Components/General/Icons";
 import CartContent from "../Components/Cart/CartContent";
+import { useNavigate } from "react-router-dom";
 
 function Cart() {
+	const navigate = useNavigate();
 	return (
 		<div>
-			<StyledLink to="/">
-				<Close />
-			</StyledLink>
+			<Close onClick={() => navigate(-1)} />
 			<CartContent />
 		</div>
 	);
