709ebbfbd6f26b78ec1465c608ed057014fff123
Adrian Rodriguez <rodriguezcadr@gmail.com>
2022-12-13 20:27:44 -0300

    mailto contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,14 +10,24 @@ import {
 } from "./ContactStyles";
 
 function Contact() {
+	const sendEmail = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const contactData = Object.fromEntries(formData);
+		if (!contactData.subject || !contactData.name || !contactData.text) {
+			return alert("Debes llenar todos los campos");
+		}
+		window.location.href = `mailto:rodriguezcadr@gmail.com?subject=${contactData.subject}&body=${contactData.text}`;
+	};
+
 	return (
 		<ContactContainer id="contact">
 			<ContactTitle>Contáctanos</ContactTitle>
 			<GlobalSubtitles>Dejanos un e-mail</GlobalSubtitles>
-			<ContactForm>
-				<ContactInput type="text" placeholder="Nombre" />
-				<ContactInput type="text" placeholder="Asunto" />
-				<Text placeholder="Escriba aquí" />
+			<ContactForm onSubmit={sendEmail}>
+				<ContactInput type="text" placeholder="Nombre" name="name" />
+				<ContactInput type="text" placeholder="Asunto" name="subject" />
+				<Text placeholder="Escriba aquí" name="text" />
 				<SendButton>Enviar</SendButton>
 			</ContactForm>
 		</ContactContainer>
